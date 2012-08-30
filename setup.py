51ae5f3404c32e9253f5c5c7a68b558c555a63ea
Laurent Peuch <cortex@worlddomination.be>
2012-08-31 00:19:56 +0200

    [doc] add descriptions in the setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,7 @@ setup(
     author_email='cortex@worlddomination.be',
     url='https://github.com/Psycojoker/django-template-fragments',
     packages=('fragments',),
+    description="helper for templates used in javascript client framework: allow to store small django templates in a dir and generate a javascript object that contains them all as strings",
+    long_description=open("README", "r").read(),
     license='MIT',
 )
