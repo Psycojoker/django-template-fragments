81314e4fad84a44531dc9ba1cf51ab93cf68482e
Laurent Peuch <cortex@worlddomination.be>
2012-09-01 06:15:15 +0200

    [mod] add templatetags dir in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,10 @@ setup(
     author='Laurent Peuch',
     author_email='cortex@worlddomination.be',
     url='https://github.com/Psycojoker/django-template-fragments',
-    packages=('fragments',),
+    packages=(
+        'fragments',
+        'fragments.templatetags',
+    ),
     description="helper for templates used in javascript client framework: allow to store small django templates in a dir and generate a javascript object that contains them all as strings",
     long_description=open("README", "r").read(),
     license='MIT',
